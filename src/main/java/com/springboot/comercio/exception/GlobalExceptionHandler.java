ac4c616cb5ff61f42de172451d440041331ba809
nkgabriiel <gabriel.3930@gmail.com>
2026-08-12 16:34:16 -0300

    fix: mapped the new exception and override validationException

--- src/main/java/com/springboot/comercio/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/comercio/exception/GlobalExceptionHandler.java
@@ -2,17 +2,22 @@ package com.springboot.comercio.exception;
 
 import com.springboot.comercio.dto.response.ErrorResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ClienteNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleNotFound(
@@ -21,18 +26,17 @@ public class GlobalExceptionHandler {
                 .body(ErrorResponseDTO.of(HttpStatus.NOT_FOUND, ex.getMessage(), request.getRequestURI()));
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleValidation(
-            MethodArgumentNotValidException ex,
-            HttpServletRequest request) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
 
-        String message =ex.getBindingResult().getFieldErrors().stream()
-                .map(erro -> erro.getField()+ ": " + erro.getDefaultMessage())
+        String message = ex.getBindingResult().getFieldErrors().stream()
+                .map(erro -> erro.getField() + ": " + erro.getDefaultMessage())
                 .collect(Collectors.joining("; "));
 
+        String path = ((ServletWebRequest) request).getRequest().getRequestURI();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ErrorResponseDTO.of(HttpStatus.BAD_REQUEST, message, request.getRequestURI()));
-
+                .body(ErrorResponseDTO.of(HttpStatus.BAD_REQUEST, message, path));
     }
 
     @ExceptionHandler(AuthenticationException.class)
@@ -42,6 +46,12 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ErrorResponseDTO.of(HttpStatus.UNAUTHORIZED, ex.getMessage(), request.getRequestURI()));
+    }
+
+    @ExceptionHandler (InvalidUserRequestData.class)
+    public ResponseEntity<ErrorResponseDTO> handleConflict(InvalidUserRequestData ex, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(ErrorResponseDTO.of(HttpStatus.CONFLICT, ex.getMessage(), request.getRequestURI()));
     }
 
     @ExceptionHandler(Exception.class)
