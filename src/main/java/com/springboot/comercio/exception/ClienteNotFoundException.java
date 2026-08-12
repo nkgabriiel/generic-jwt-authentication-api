0c5104adb0fdeb2f96cab04a2d1907974b8218c2
nkgabriiel <gabriel.3930@gmail.com>
2026-08-12 16:33:37 -0300

    fix: ClienteNotFoundException super message

--- src/main/java/com/springboot/comercio/exception/ClienteNotFoundException.java
+++ src/main/java/com/springboot/comercio/exception/ClienteNotFoundException.java
@@ -1,7 +1,7 @@
 package com.springboot.comercio.exception;
 
 public class ClienteNotFoundException extends RuntimeException {
-    public ClienteNotFoundException(Long id) {
-        super("Cliente não encontrado com o ID: " + id);
+    public ClienteNotFoundException(String message) {
+        super(message);
     }
 }
