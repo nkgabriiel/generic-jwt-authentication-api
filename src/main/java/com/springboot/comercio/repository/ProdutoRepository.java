d3a47decefddc006a0f4bca4c1333131887940b8
nkgabriiel <gabriel.3930@gmail.com>
2026-08-19 17:43:26 -0300

    feat: update ProdutoRepository for new stock management query

--- src/main/java/com/springboot/comercio/repository/ProdutoRepository.java
+++ src/main/java/com/springboot/comercio/repository/ProdutoRepository.java
@@ -2,7 +2,14 @@ package com.springboot.comercio.repository;
 
 import com.springboot.comercio.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     boolean existsByNome(String nome);
+
+    @Modifying
+    @Query("UPDATE Produto p SET p.estoque = p.estoque - :quantidade WHERE p.id = :id AND p.estoque >= :quantidade")
+    int baixarEstoqueSeSuficiente(@Param("id") Long id, @Param("quantidade") Integer quantidade);
 }
